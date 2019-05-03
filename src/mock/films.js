aa8ef269389c0f90fbc2905a264f681e3c8255e2
maxm <lynchnost@gmail.com>
2019-05-03 22:26:04 +0300

    module2-task1 done

---
+++ src/mock/films.js
@@ -0,0 +1,97 @@
+export const films = [
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    picUrl: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Bohemian Rhapsody`,
+    picUrl: `img/bohemian-rhapsody.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Macbeth`,
+    picUrl: `img/macbeth.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Aviator`,
+    picUrl: `img/aviator.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `We need to talk about Kevin`,
+    picUrl: `img/we-need-to-talk-about-kevin.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `What We Do in the Shadows`,
+    picUrl: `img/what-we-do-in-the-shadows.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Revenant`,
+    picUrl: `img/revenant.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Johny English`,
+    picUrl: `img/johnny-english.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Shutter Island`,
+    picUrl: `img/shutter-island.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Pulp Fiction`,
+    picUrl: `img/pulp-fiction.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `No Country for Old Men`,
+    picUrl: `img/no-country-for-old-men.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Snatch`,
+    picUrl: `img/snatch.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Moonrise Kingdom`,
+    picUrl: `img/moonrise-kingdom.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Seven Years in Tibet`,
+    picUrl: `img/seven-years-in-tibet.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Midnight Special`,
+    picUrl: `img/midnight-special.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `War of the Worlds`,
+    picUrl: `img/war-of-the-worlds.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Dardjeeling Limited`,
+    picUrl: `img/dardjeeling-limited.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Orlando`,
+    picUrl: `img/orlando.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+  {
+    name: `Mindhunter`,
+    picUrl: `img/mindhunter.jpg`,
+    pageUrl: `https://thepiratebay.org`
+  },
+];
