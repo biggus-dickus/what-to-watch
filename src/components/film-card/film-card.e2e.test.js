3561895c5b6944d411fb29c8e3153e50eca1110c
maxm <lynchnost@gmail.com>
2019-05-27 22:05:47 +0300

    module4-task1: fixes film-card e2e test

--- src/components/film-card/film-card.e2e.test.js
+++ src/components/film-card/film-card.e2e.test.js
@@ -10,17 +10,20 @@ import Video from '../video-player/video-player';
 configure({adapter: new Adapter()});
 
 
-describe(`FilmCard test suite.`, () => {
-  const movieCard = shallow(<FilmCard {...mockFilms[0]} />);
-  const video = movieCard.find(Video);
+const movieCard = shallow(<FilmCard {...mockFilms[0]} />);
 
+describe(`FilmCard test suite.`, () => {
   it(`Hovering over FilmCard changes 'isHovered' status to true`, () => {
     movieCard.simulate(`mouseenter`);
+
+    const video = movieCard.find(Video);
     expect(video.props().isHovered).toBe(true);
   });
 
   it(`Moving the mouse away from FilmCard changes 'isHovered' status to false`, () => {
     movieCard.simulate(`mouseleave`);
+
+    const video = movieCard.find(Video);
     expect(video.props().isHovered).toBe(false);
   });
 });
