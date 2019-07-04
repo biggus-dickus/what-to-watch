172f6d4b1b284382d71692fed21f93537394226c
maxm <maxm@nastasha.com>
2019-07-04 10:35:53 +0300

    completes webpack-prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const CopyPlugin = require(`copy-webpack-plugin`);
 const merge = require(`webpack-merge`);
 const path = require(`path`);
 
@@ -9,5 +10,21 @@ module.exports = merge(common, {
   output: {
     path: path.join(__dirname, `build`),
     filename: `bundle.min.js`,
-  }
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: `public/css/style.min.css`,
+        to: path.resolve(__dirname, `build/css`)
+      },
+      {
+        from: `public/img`,
+        to: path.resolve(__dirname, `build/img`)
+      },
+      {
+        from: `public/favicon.ico`,
+        to: path.resolve(__dirname, `build`)
+      }
+    ])
+  ]
 });
